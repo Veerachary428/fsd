aecbe4730c39824f05ad29d1c3a4a1b73d1928ed
Veerachary428 <veerachary4679@gmail.com>
2025-03-27 15:54:23 +0530

    all files

---
+++ test.js
@@ -0,0 +1,42 @@
+//comparison operators
+console.log("1" == 1)
+//double equals to implies the loose equals too; only comapares values
+// strict equals to implies strict data comaparison ; compares both values and the type of the data types
+
+
+
+let a=6; //intializing the variable
+
+console.log(a); //printing the variable
+console.log(3+4);
+
+//1) number
+//2) String
+//3) Boolean
+// 4) null
+//5) Undefined
+// /**/ mutli line customElements
+
+
+//syntax of for loop
+for(let i =0 ; i > -5 ; i--){
+    console.log(i);
+}
+
+
+
+// it is the counter 
+// let countValue = document.getElementById("container");
+        // function dec(){
+        //     c=b-c-1;
+        //     countValue.textContent=c;
+        //     if(c%2!=0){
+        //        pass 
+        //     }
+        //     else if(c==0){
+        //         pass
+        //     }
+        //     else{
+        //         pass
+        //     }
+        // }
